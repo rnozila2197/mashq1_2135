21a378080b35eeb6761f95e309deb98f61fd60a3
rnozila2197 <yjamshid2197@gmail.com>
2025-11-14 14:56:31 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,35 @@
+#1
+matn = " Python dasturlash "
+print(len(matn))
+print(matn.lstrip())
+
+#2
+matn = "Hello World"
+print(matn.upper())
+print(len(matn))
+
+#3
+matn = "SALOM DUNYO 123"
+print(matn.lower())
+print(matn.strip())
+
+#4
+matn = " ozbekiston "
+print(matn.rstrip())
+print(matn.capitalize())
+
+#5
+matn = "python3.11"
+print(matn.title())
+print(len(matn * 2))
+
+#6
+matn = " Toshkent 2025 "
+print(matn.strip())
+print(matn.upper())
+
+#7
+matn = "ALGORITM VA DASTURLASH"
+print(matn.lower())
+print(matn.lstrip())
+print(len(matn))
